073243b8292aade22442a85f121cd253e1b13c22
nmuravkov-alt <nmuravkov@gmail.com>
2025-10-17 22:05:08 +0300

    Update runner.py

--- runner.py
+++ runner.py
@@ -1,18 +1,5 @@
-# runner.py
 import asyncio
-import logging
-
-from main import _run as run_bot
-from scheduler import run_scheduler
-
-logging.basicConfig(level=logging.INFO)
-
-async def _run():
-    # Запускаем бот и планировщик параллельно
-    await asyncio.gather(
-        run_bot(),
-        run_scheduler(),
-    )
+from main import _run
 
 if __name__ == "__main__":
     asyncio.run(_run())
