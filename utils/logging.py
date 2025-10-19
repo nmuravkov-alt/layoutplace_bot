0255404b07e83dbfb12b4d21aceabdd90c768ccc
nmuravkov-alt <nmuravkov@gmail.com>
2025-10-19 20:27:52 +0300

    Delete utils directory

--- utils/logging.py
+++
@@ -1,3 +0,0 @@
-import logging, sys
-logging.basicConfig(level=logging.INFO, format="%(asctime)s | %(levelname)s | %(name)s | %(message)s", handlers=[logging.StreamHandler(sys.stdout)])
-logger = logging.getLogger("layoutplace-bot")
